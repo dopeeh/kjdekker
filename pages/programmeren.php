4afdfde692c26bc4d8278c5214a1b5b6fdf31def
Koen Jesse Dekker <dopeehedit@gmail.com>
2019-10-09 19:04:44 +0200

    dark bg

--- pages/programmeren.php
+++ pages/programmeren.php
@@ -8,11 +8,11 @@
         
         <!-- Page content -->
         <div id='programmingParagraph' class='hidden'>
-            <p>&emsp;Alhoewel programmeren een relatief nieuwe hobby van mij is, heb ik er snel een passie voor ontwikkeld. Toen ik mijn eindstage liep bij Warbout Distributie 
+            <p>Alhoewel programmeren een relatief nieuwe hobby van mij is, heb ik er snel een passie voor ontwikkeld. Toen ik mijn eindstage liep bij Warbout Distributie 
             (Netwerkbeheer MBO, afgerond in 2018) kreeg ik de mogelijkheid om in contact te komen met programmeren. Dit heb ik sindsdien niet meer losgelaten. Ik ben dan ook een opleiding 
             gaan volgen bij de <a href=''>Hanzehogeschool Groningen</a> om deze passie verder te onderzoeken en mij te ontwikkelen binnen deze nieuwe wereld.</p>
 
-            <p>&emsp;Bij een project werk ik graag naar een doel en zie ik problemen als uitdagingen die mij van dat doel weerhouden. Niets is onmogelijk en met genoeg geduld valt alles op te lossen. 
+            <p>Bij een project werk ik graag naar een doel en zie ik problemen als uitdagingen die mij van dat doel weerhouden. Niets is onmogelijk en met genoeg geduld valt alles op te lossen. 
             Dit sluit perfect aan bij de manier waarop de logica in elkaar steekt binnen de verschillende programmeertalen. Omdat mijn denkwijze aansluit bij de manier waarop de problemen opgelost moeten worden 
             heb ik ondervonden dat ik flexibel tussen programmeertalen kan switchen. Dit heeft natuurlijk als nadeel dat ik mij niet heb kunnen specialiseren.</p>
 
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $string = 'kdekker@colourframe';
-
-    if(filter_var($string, FILTER_VALIDATE_EMAIL)) {
-        echo "<h1>Dikke success a mattie</h1>";
-    } else {
-        echo "invalid input";
-    }
-?>
