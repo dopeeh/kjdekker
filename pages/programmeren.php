500dd101540cedcdff5a4b668564c4671141ccfe
dopeeh <38014435+dopeeh@users.noreply.github.com>
2020-02-01 15:17:37 +0100

    Updated some tekst

--- pages/programmeren.php
+++ pages/programmeren.php
@@ -14,9 +14,9 @@
         
         <!-- Page content -->
         <div id='programmingParagraph' class='hidden'>
-            <p>Alhoewel programmeren een relatief nieuwe hobby van mij is, heb ik er snel een passie voor ontwikkeld. Toen ik mijn eindstage liep bij Warbout Distributie 
-            (Netwerkbeheer MBO, afgerond in 2018) kreeg ik de mogelijkheid om in contact te komen met programmeren. Dit heb ik sindsdien niet meer losgelaten. Ik ben dan ook een opleiding 
-            gaan volgen bij de <a href=''>Hanzehogeschool Groningen</a> om deze passie verder te onderzoeken en mij te ontwikkelen binnen deze nieuwe wereld.</p>
+            <p>Programmeren is een relatief nieuwe hobby van mij. Toen ik mijn eindstage liep bij Warbout Distributie 
+            (Netwerkbeheer MBO, met diploma afgerond in 2018) kreeg ik de mogelijkheid om in contact te komen met programmeren. Dit heb ik sindsdien niet meer losgelaten. Ik ben dan ook een opleiding 
+            gaan volgen bij de <a href='https://www.hanze.nl'>Hanzehogeschool Groningen</a> om deze passie verder te onderzoeken en mij te ontwikkelen binnen deze nieuwe wereld.</p>
 
             <p>Bij een project werk ik graag naar een doel en zie ik problemen als uitdagingen die mij van dat doel weerhouden. Niets is onmogelijk en met genoeg geduld valt alles op te lossen. 
             Omdat mijn denkwijze aansluit bij de manier waarop de problemen opgelost moeten worden heb ik ondervonden dat ik flexibel tussen programmeertalen kan switchen. 
@@ -33,6 +33,8 @@
         <button class='btn btn-outline-dark tldr-button' onClick="tldr('programmingParagraph')">
             Meer lezen...
         </button>
+
+        <p class='mt-5'>Portfolio:</p>
 
     </div>
 </div>
